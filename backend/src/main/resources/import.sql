0a376be692ca176f2e0d3e72f710a7cfb719ecf6
Leandro Arakaki <leanarakaki@gmail.com>
2020-10-29 19:52:28 -0400

    Correct file import.sql

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,3 @@
 INSERT INTO TB_CATEGORY (name) VALUES ('Books');
 INSERT INTO TB_CATEGORY (name) VALUES ('Eletronics');
-INSERT INTO TB_CATEGORY (name) VALUES ('Computes');
+INSERT INTO TB_CATEGORY (name) VALUES ('Computers');
