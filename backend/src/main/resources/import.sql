9d0f4e5c87ee9abb001dbe8c5fd772ddfa272a36
Leandro Arakaki <leanarakaki@gmail.com>
2020-10-31 16:03:14 -0400

    Pagination

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,19 @@
 INSERT INTO TB_CATEGORY (name, create_At) VALUES ('Books', NOW());
 INSERT INTO TB_CATEGORY (name, create_At) VALUES ('Eletronics', NOW());
 INSERT INTO TB_CATEGORY (name, create_At) VALUES ('Computers', NOW());
+
+INSERT INTO TB_CATEGORY (name, create_At) VALUES ('Books', NOW());
+INSERT INTO TB_CATEGORY (name, create_At) VALUES ('Eletronics', NOW());
+INSERT INTO TB_CATEGORY (name, create_At) VALUES ('Computers', NOW());
+
+INSERT INTO TB_CATEGORY (name, create_At) VALUES ('Books', NOW());
+INSERT INTO TB_CATEGORY (name, create_At) VALUES ('Eletronics', NOW());
+INSERT INTO TB_CATEGORY (name, create_At) VALUES ('Computers', NOW());
+
+INSERT INTO TB_CATEGORY (name, create_At) VALUES ('Books', NOW());
+INSERT INTO TB_CATEGORY (name, create_At) VALUES ('Eletronics', NOW());
+INSERT INTO TB_CATEGORY (name, create_At) VALUES ('Computers', NOW());
+
+INSERT INTO TB_CATEGORY (name, create_At) VALUES ('Books', NOW());
+INSERT INTO TB_CATEGORY (name, create_At) VALUES ('Eletronics', NOW());
+INSERT INTO TB_CATEGORY (name, create_At) VALUES ('Computers', NOW());
