f5a292d4d6469d217d2a6ecc5497dd0a9731fc82
Leandro Arakaki <leanarakaki@gmail.com>
2020-12-13 16:42:56 -0400

    project profiles

--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserUpdateDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserUpdateDTO.java
@@ -1,13 +1,9 @@
 package com.devsuperior.dscatalog.dto;
 
 import com.devsuperior.dscatalog.services.validation.UserUpdateValid;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 @UserUpdateValid
 public class UserUpdateDTO extends UserDTO {
 
